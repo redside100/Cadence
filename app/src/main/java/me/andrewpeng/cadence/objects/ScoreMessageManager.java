1ad2bf2178d62b0bc06a6086b30949ea90cca1b5
Isaac Leung <isaac1937@gmail.com>
2018-05-07 10:38:41 -0400

    Pull

--- app/src/main/java/me/andrewpeng/cadence/objects/ScoreMessageManager.java
+++ app/src/main/java/me/andrewpeng/cadence/objects/ScoreMessageManager.java
@@ -1,8 +1,34 @@
 package me.andrewpeng.cadence.objects;
 
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.view.MotionEvent;
+
+import java.util.ArrayList;
+
 /**
- * Created by zacchaeusleung on 2018-05-07.
+ * Created by isaacleung on 2018-05-07.
  */
 
 public class ScoreMessageManager {
+    public static ArrayList<ScoreMessage> scoreMessages = new ArrayList<>();
+    public static void render(Canvas graphics, Paint paint) {
+        ArrayList<ScoreMessage> temp = new ArrayList<>(scoreMessages);
+        for(ScoreMessage scoreMessage: temp) {
+            scoreMessage.tick();
+        }
+    }
+
+    public static void tick() {
+
+    }
+
+    public static void touch(MotionEvent e) {
+        try {
+
+        }catch (ArrayIndexOutOfBoundsException e1) {
+
+        }
+    }
+
 }
