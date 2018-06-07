456990e3fbdd538dc8ea4cbf0f26753bc7a713dd
Isaac Leung <isaac1937@gmail.com>
2018-06-07 08:56:38 -0400

    Fix block things

--- app/src/main/java/me/andrewpeng/cadence/core/MainView.java
+++ app/src/main/java/me/andrewpeng/cadence/core/MainView.java
@@ -63,7 +63,6 @@ public class MainView extends View implements GestureDetector.OnGestureListener 
 
         // Load beatmaps (add twice for now, to get a full list)
         conductor.initBeatmaps();
-<<<<<<< HEAD
         conductor.initBeatmaps();
 
         init();
@@ -130,8 +129,6 @@ public class MainView extends View implements GestureDetector.OnGestureListener 
         }catch (IOException e) {
 
         }
-=======
->>>>>>> 810aefcff0b0f09d9f4b7d7a24da7969b7612b65
     }
 
 
